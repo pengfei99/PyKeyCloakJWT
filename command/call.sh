42316b26c8eb7c03dd15a853c9ded68c7a0e35c8
Pengfei Liu <liu.pengfei@hotmail.fr>
2022-05-15 21:39:14 +0200

    update

--- command/call.sh
+++ command/call.sh
@@ -1,9 +1,10 @@
 # ./get-kc-jwt-token.sh localhost:8080 pengfei-test toto pengfei-dv-app
 
 REALM_NAME=pengfei-test
-HOSTNAME=localhost:8080
-USERNAME=toto
+HOSTNAME=127.0.0.1:8080
+USERNAME=pengfei
 CLIENT_ID=pengfei-dv-app
+CLIENT_SECRET=enifviJDIpbN5230yfcPo7h2zsifTa2z
 PASSWORD=toto
 
 KEYCLOAK_URL=http://$HOSTNAME/auth/realms/$REALM_NAME/protocol/openid-connect/token
@@ -14,4 +15,5 @@ curl -X POST "$KEYCLOAK_URL" "--insecure" \
  -d "username=$USERNAME" \
  -d "password=$PASSWORD" \
  -d 'grant_type=password' \
- -d "client_id=$CLIENT_ID"
+ -d "client_id=$CLIENT_ID" \
+ -d "client_secret=$CLIENT_SECRET"
