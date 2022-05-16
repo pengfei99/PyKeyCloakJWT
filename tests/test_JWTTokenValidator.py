1829374bcfac8ce1bf841b8b5eb4520028338ac1
pengfei <liu.pengfei@hotmail.fr>
2022-05-16 11:32:34 +0200

    update test

--- tests/test_JWTTokenValidator.py
+++ tests/test_JWTTokenValidator.py
@@ -1,10 +1,25 @@
+import calendar
+import time
+
 import pytest
 
 from creds import ssp_token
 from src.JWTTokenValidator import JWTTokenValidator
 
 
-def test_is_expired():
-    validator = JWTTokenValidator()
-    actual_val = validator.is_expired(ssp_token)
+@pytest.fixture
+def token_validator():
+    return JWTTokenValidator()
+
+
+def test_is_expired(token_validator):
+    actual_val = token_validator.is_expired(ssp_token)
     assert actual_val == False
+
+
+def test_get_current_unix_ts(token_validator):
+    expected_ts = calendar.timegm(time.gmtime())
+    actual_ts = token_validator.get_current_unix_ts()
+    print(f"expected value: {expected_ts}")
+    print(f"actual value: {actual_ts}")
+    assert actual_ts == expected_ts
